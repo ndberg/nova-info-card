4df57055bb838d18f4b6bded28d327bedb0afbb9
Daniel Hummerich <d.hummerich@rmh-media.com>
2023-09-04 18:05:56 +0200

    fix(tailwind): removed tailwind due conflicts with other tailwind projects

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,12 +1,6 @@
 let mix = require('laravel-mix')
-const tailwindcss = require('tailwindcss')
 
 mix
     .setPublicPath('dist')
     .js('resources/js/card.js', 'js')
     .vue({version: 3})
-    .postCss('resources/css/card.css', 'dist/css')
-    .options({
-        processCssUrls: false,
-        postCss: [tailwindcss('./tailwind.config.js')]
-    })
